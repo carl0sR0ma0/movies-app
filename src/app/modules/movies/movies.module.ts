951c43e4c83dcada7f79990cb7783306cb6583f4
carl0sR0ma0 <cvpromao@gmail.com>
2020-09-12 10:43:46 -0300

    Fazendo as importações necessárias para trabalhar com formulários.

--- src/app/modules/movies/movies.module.ts
+++ src/app/modules/movies/movies.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies.component';
@@ -24,7 +30,13 @@ import { ComponentsModule } from './../../components/components.module';
     MatButtonModule,
     MatCardModule,
     FlexLayoutModule,
-    ComponentsModule
+    ComponentsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule    
   ]
 })
 export class MoviesModule { }
